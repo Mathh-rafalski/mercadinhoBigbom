fa5b787e44582c84d06cf232616b72dfc99747c0
Matheus <matheus__rafalski@hotmail.com>
2020-03-12 22:39:27 -0300

    'vish'

--- index.js
+++ index.js
@@ -17,11 +17,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'))
 
-
+app.get('/getVendas', function (req, res) {
+  connection.query('select id,data_hora from vendas',
+    function (error, results, fields) {
+      if (error)
+        res.json(error);
+      else
+        res.json(results);
+      //connection.end();
+    });
+  });
 app.get('/produto/:id', function (req, res) {
   let id = req.params.id;
   console.log(id)
-  let sql = 'select nome,valor from produtos where id=?'
+  let sql = 'select id,nome,valor from produtos where id=?'
   connection.query(sql, id, (error, results, fields) => {
     if (error) {
       console.log("erro do bao")
@@ -29,6 +38,34 @@ app.get('/produto/:id', function (req, res) {
     res.json(results)
   });
 });
+app.post('/postVenda', function(req,res) {
+  let venda_id;
+  var sql = "INSERT INTO `vendas` (data_hora) VALUES ('" + req.body.dataHora+ "')";
+  connection.query(sql, function (err, result) {
+    res.send("fala fi")
+  });
+});
+app.post('/postVendaItem', function(req,res) {
+  var data = Date();
+  connection.query("insert into vendas (data_hora) values ('"+data+"')",function(err,result) {
+
+  })
+  connection.query("select id from vendas ",function(err,results,fields) {
+    venda_id = results
+  })
+  console.log(req.body.json)
+  var sql = "INSERT INTO `produto_venda` (produto_id,venda_id,quantidade,valor_total) VALUES ('" + req.body.produto_id + "', '" + venda_id + "','"+req.body.quantidade+"','"+req.body.valor_total+"')";
+  connection.query(sql, function (err, result) {
+    res.send("fala fi")
+  });
+});
+/*BEGIN;
+INSERT INTO users (username, password)
+  VALUES('test', 'test');
+INSERT INTO profiles (userid, bio, homepage) 
+  VALUES(LAST_INSERT_ID(),'Hello world!', 'http://www.stackoverflow.com');
+COMMIT;
+*/
 app.get('/getProdutos', function (req, res) {
   connection.query('select nome,valor,id from produtos',
     function (error, results, fields) {
@@ -41,6 +78,10 @@ app.get('/getProdutos', function (req, res) {
   });
   app.use('/cadProduto', function (req, res) {
     res.sendFile(path.join(__dirname, 'public', 'Novoproduto.html'));
+    //C:\Users\Matheus\Desktop\an
+  });
+  app.use('/cadVenda', function (req, res) {
+    res.sendFile(path.join(__dirname, 'public', 'cadVenda.html'));
     //C:\Users\Matheus\Desktop\an
   });
   app.post('/postProd', function (req, res) {
