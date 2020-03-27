00b658b4f9a20050d90b6701c129b44b8411545b
Matthew <55326761+matheus__rafalski@users.noreply.github.com>
2020-03-26 23:25:08 -0300

    ainda falta o filtro de vendas

--- index.js
+++ index.js
@@ -20,43 +20,45 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'))
 
 app.get('/vendas', function (req, res) {
-  
-  
+
+
   let datade = req.query.datade
-  console.log(datade);
-  let dataate = req.query.dataate 
+  let dataate = req.query.dataate
   datade = moment(datade).format('YYYY-MM-DD')
   dataate = moment(dataate).format('YYYY-MM-DD')
-  console.log('2 '+dataate)
-  console.log('1 '+datade);
-  
-  connection.query(`select id,quantidade,venda_id,(select data_hora from vendas v where v.id = pv.venda_id and data_hora > ${datade} and data_hora < ${dataate}) as data_hora,valor_total from produto_venda pv order by data_hora desc`,
-  (erro,results,fields) => {
+  console.log('dataate ' + dataate)
+  console.log('datede ' + datade);
 
-  
-  if (erro) {
-    res.send({erro:'errro'})
-  }
-  results.forEach(element => {
-    console.log(element);
-    
-  })
-  res.json(results)
-})
+  // connection.query(`select id,quantidade,venda_id,(select data_hora from vendas v where v.id = pv.venda_id and data_hora > ${datade} and data_hora < ${dataate}) as data_hora,valor_total from produto_venda pv order by data_hora desc`,
+  connection.query(` select v.id,data_hora,sum(quantidade) as quantidade,sum(valor_total) as valor_total from vendas v join produto_venda pv on (pv.venda_id = v.id) where data_hora between ${datade} and ${dataate} group by v.id`,
+    (erro, results, fields) => {
+
+
+      if (erro) {
+        res.send({ erro: 'errro' })
+      }
+      results.forEach(element => {
+        console.log(element);
+
+        element.data_hora = moment(element.data_hora).format('DD/MM/YYYY HH:mm')
+
+      })
+      res.json(results)
+    })
 })
 
-app.delete('/deletar/:id',function(req,res) {
+app.delete('/deletar/:id', function (req, res) {
   let id = req.params.id;
   let sql = `delete from produtos where id = ?`
   console.log("ou")
   connection.query(sql, [id], (error, results, fields) => {
-      if (error) {
-        res.status(500).json({erro:'erro'})
-      } else {
-        res.send('del')
-      }
-      
-});
+    if (error) {
+      res.status(500).json({ erro: 'erro' })
+    } else {
+      res.send('del')
+    }
+
+  });
 });
 /*
 SELECT nome,(select sum(quantidade) from produto_venda pv where p.id = pv.produto_id)*100/(select sum(quantidade) from produto_venda) as porcentagem FROM produto_venda pv2
@@ -64,10 +66,10 @@ join produtos p on(p.id = pv2.produto_id)
 group by nome;
 
 */
-app.get('/graph',function(req,res){
-  let sql = "select nome,(select sum(quantidade) from produto_venda pv where p.id = pv.produto_id)*100/(select sum(quantidade) from produto_venda) as porcentagem from produto_venda pv2 join produtos p on (p.id = pv2.produto_id) group by nome order by porcentagem desc limit 5;"
-  connection.query(sql,function(error,results,fields){
-    if(error) {
+app.get('/graph', function (req, res) {
+  let sql = "select nome,(select sum(quantidade) from produto_venda pv where p.id = pv.produto_id)*100/(select sum(quantidade) from produto_venda) as porcentagem,(select sum(valor_total) from produto_venda) as total,(select sum(valor_total) from produto_venda where p.id = produto_id) as valor_produto from produto_venda pv2 join produtos p on (p.id = pv2.produto_id) group by nome order by porcentagem desc limit 5"
+  connection.query(sql, function (error, results, fields) {
+    if (error) {
       console.log(error)
     }
     console.log('fort')
@@ -77,14 +79,14 @@ app.get('/graph',function(req,res){
 
 // update produtos set categoria_id=8 where categoria_id = 2 or categoria_id = 7;
 // update tarefas set descricao = 'aaaa' where id='24'
-app.post("/produto/update",function(req,res) {
+app.post("/produto/update", function (req, res) {
   console.log('eu sie')
   let nome = req.body.nome
   let id = req.body.id;
   let valor = req.body.valor
-  valor = validarCampos(valor,nome,res)
+  valor = validarCampos(valor, nome, res)
   console.log(boolean)
-  if(boolean == false) {
+  if (boolean == false) {
     return;
   }
   /*desc = desc.replace("+","'")
@@ -92,20 +94,20 @@ app.post("/produto/update",function(req,res) {
   dataHora = dataHora.replace("+","'")
   dataHora = dataHora.replace("+","'")*/
   let sql = `update produtos set valor=?,nome=? where id=?`
-  connection.query(sql,[valor,nome,id],(error,results,fields) => {
-      if (error) {
-          console.log("umas treta loca ai")
-      }
-      res.json({erro:'salvo'})
+  connection.query(sql, [valor, nome, id], (error, results, fields) => {
+    if (error) {
+      console.log("umas treta loca ai")
+    }
+    res.json({ erro: 'salvo' })
   });
 });
 
 // select id,quantidade,venda_id,(select data_hora from vendas v where v.id = venda_id),valor_total from produto_venda pv
 app.get('/getVendas', function (req, res) {
   console.log(req.body.obj);
-  
+
   let select = `(select data_hora from vendas v where v.id = venda_id )`
-  connection.query(`select concat('2020/',extract(month from current_date),'/1') as data_de,concat('2020/',extract(month from current_date),'/31') as data_ate,id,quantidade,venda_id,(select data_hora from vendas v where v.id = venda_id and data_hora > data_de and data_hora < data_ate) as data_hora,valor_total from produto_venda pv order by data_hora desc`,
+  connection.query(`select data_hora,v.id,sum(quantidade) as quantidade,sum(valor_total) as valor_total from vendas v join produto_venda pv on(pv.venda_id = v.id) where extract(month from current_date) = extract(month from data_hora) and extract(year from current_date) = extract(year from data_hora) group by v.id order by data_hora desc`,
     function (error, results, fields) {
       if (error)
         res.json(error);
@@ -113,7 +115,7 @@ app.get('/getVendas', function (req, res) {
         results.forEach(element => {
           element.data_hora = moment(element.data_hora).format('DD/MM/YYYY HH:mm')
         });
-        res.json(results);
+      res.json(results);
       //connection.end();
     });
 });
@@ -125,7 +127,7 @@ app.get('/produto/:id', function (req, res) {
     if (error) {
       console.log("erro do bao")
     }
-    if(results[0] == null) {
+    if (results[0] == null) {
       res.json("vazio")
       return;
     }
@@ -148,9 +150,9 @@ app.post('/postVendaItem', function (req, res) {
     var venda_id = result.insertId
     var array = req.body.itens
     for (let i = 0; i < array.length; i++) {
-      var select = "(select valor from produtos where id = '" +array[i].produto_id+"')*'"+array[i].quantidade+"'"
+      var select = "(select valor from produtos where id = '" + array[i].produto_id + "')*'" + array[i].quantidade + "'"
       // INSERT INTO produto_venda (produto_id,venda_id,quantidade,valor_total) VALUES ('1','1','3', (select valor from produtos where id = '1')*3)
-      var sql = "INSERT INTO `produto_venda` (produto_id,venda_id,quantidade,valor_total) VALUES ('" + array[i].produto_id + "', '" + venda_id + "','" + array[i].quantidade + "'," +select + ")";
+      var sql = "INSERT INTO `produto_venda` (produto_id,venda_id,quantidade,valor_total) VALUES ('" + array[i].produto_id + "', '" + venda_id + "','" + array[i].quantidade + "'," + select + ")";
       connection.query(sql, function (err, result) {
         console.log(err)
       });
@@ -161,46 +163,46 @@ app.post('/produto/inserir', function (req, res) {
   let n = req.body.valor
   let p = req.body.nome
   boolean = true;
-  n = validarCampos(n,p,res)
+  n = validarCampos(n, p, res)
   console.log(boolean);
-  
+
   if (boolean == false) {
     return;
   }
   console.log('a to');
-  
+
   var sql = "INSERT INTO `produtos` (`nome`,`valor`) VALUES ('" + p + "', '" + n + "')";
   connection.query(sql, function (err, result) {
-    res.send({erro:'salvo'})
+    res.send({ erro: 'salvo' })
   });
 });
 var boolean = true;
-function validarCampos(n,p,res) {
+function validarCampos(n, p, res) {
   let b = true
   if (!isNaN(n)) {
     console.log('eror nu')
     boolean = false
-    res.status(500).send({erro:'Valor inválido'})
+    res.status(500).send({ erro: 'Valor inválido' })
     return;
   }
   if (p == '' || p.length < 2) {
     console.log('o')
     boolean = false
-    res.status(500).send({erro:'Nome não pode ser nulo'})
+    res.status(500).send({ erro: 'Nome não pode ser nulo' })
     return;
   }
   let s = p.split('')
-  for(let i = 0;i < s.length;i++) {
-    if(!isNaN(s[i])) {
+  for (let i = 0; i < s.length; i++) {
+    if (!isNaN(s[i])) {
       console.log('o')
       boolean = false
-      res.status(500).send({erro:'Não pode conter números no nome '})
+      res.status(500).send({ erro: 'Não pode conter números no nome ' })
       return;
     }
   }
-  n = n.replace('R$',"")
-  n = n.replace('.','')
-  n = n.replace(',','.')
+  n = n.replace('R$', "")
+  n = n.replace('.', '')
+  n = n.replace(',', '.')
   n = parseFloat(n).toFixed(1)
   return n;
 }
@@ -229,13 +231,13 @@ app.get('/getProdutos', function (req, res) {
         res.json(error);
         console.log(error)
       }
-        res.json(results);
+      res.json(results);
       //connection.end();
-      
+
     });
 });
-app.use('/editProduto/:id',function(req,res){
-  res.sendFile(path.join(__dirname,'public','Novoproduto.html'))
+app.use('/editProduto/:id', function (req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'Novoproduto.html'))
   /*
   let json = {
     'nome':req.params.nome,
@@ -246,11 +248,11 @@ app.use('/editProduto/:id',function(req,res){
   */
 
 })
-app.use('/topCinco', function(req,res){
-  res.sendFile(path.join(__dirname, 'public','produtosVendidos.html'))
+app.use('/topCinco', function (req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'produtosVendidos.html'))
 })
-app.use('/hori', function(req,res){
-  res.sendFile(path.join(__dirname, 'public','graphorizo.html'))
+app.use('/hori', function (req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'graphorizo.html'))
 })
 app.use('/cadProduto', function (req, res) {
   res.sendFile(path.join(__dirname, 'public', 'Novoproduto.html'));
